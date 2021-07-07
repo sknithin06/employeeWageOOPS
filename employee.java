57477c63eda07282ae126cc37230b63c75b15e22
sknithin06 <nithinsk939@gmail.com>
2021-07-07 20:40:01 +0530

    initial commit

---
+++ employee.java
@@ -0,0 +1,12 @@
+package bridgelabz;
+public class employee {
+	public static final int isFulltime = 1;
+	public static final int isParttime = 2;
+	public static final int empRatePerHrs = 20;
+	public static final int numWorkingDays = 20;
+	public static final int maxHrsMonth =10;
+	public static final int empHrs = 8;
+	public static void main(String[] args) {
+		System.out.println("Welcome to EmployeeWage");
+	}
+}
