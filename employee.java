92990a141f4d44d5a7b321f734f806c7e397404b
sknithin06 <nithinsk939@gmail.com>
2021-07-07 20:48:36 +0530

    check the employee is present or absent

--- employee.java
+++ employee.java
@@ -6,7 +6,19 @@ public class employee {
 	public static final int numWorkingDays = 20;
 	public static final int maxHrsMonth =10;
 	public static final int empHrs = 8;
+	
+	void checkEmployee () {
+		int isPresent = 1;
+		double empcheck = Math.floor(Math.random() * 10) % 2;
+		if (empcheck == isPresent)
+		 	System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+		}
 	public static void main(String[] args) {
 		System.out.println("Welcome to EmployeeWage");
+		
+		employee people = new employee();
+		people.checkEmployee();
 	}
 }
